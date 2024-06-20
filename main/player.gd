0f706c22291721320aa3c847b2b5ae14696bb862
clamlum2 <clamlum2@gmail.com>
2024-06-20 15:59:38 +1200

    Revert "basis for main menu"
    This reverts commit 4bb2c6236ce8e0c5a990efef55e17d8813543aad.

--- main/player.gd
+++ main/player.gd
@@ -2,24 +2,19 @@ extends CharacterBody3D
 
 var damage = 10
 const MAX_CAM_SHAKE = 0.05
+
 var currentspeed = 0
 var SPEED = 7.5
-var fasterspeed = 10
-var AIRSPEED = 0.1
+var AIRSPEED = 6.0
 var JUMP_VELOCITY = 4.5
 var sens = 0.00075
-#var sens = 0.005
-var gravity = 15
-var health = 200
 
+var gravity = 15
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
 @onready var anim_player = $AnimationPlayer
 @onready var camera1 = $Neck/Camera3D
 @onready var raycast = $Neck/Camera3D/RayCast3D
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func fire ():
 	if Input.is_action_pressed("fire"):
@@ -45,48 +40,41 @@ func _unhandled_input(event: InputEvent):
 			camera.rotate_x(-event.relative.y * sens)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
-func _process(delta):
-	
-	if health <= 0:
-		queue_free()
-	
-	if position.y < -100:
-		get_tree().quit()
+func _physics_process(delta):
 	
 	fire()
 	
-	if Input.is_action_pressed("restart"):
-		get_tree().change_scene_to_file("res://control.tscn")
-
-
-func _physics_process(delta):
-	
+	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_pressed("jump") and is_on_floor():
+	# Handle jump.
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var spd : float = velocity.length()
-	#print("velocity"+str(velocity))
-	#print("looking"+str(neck.get_transform().basis.x))
-	if is_on_floor() and Input.is_action_pressed("jump") and direction:
-		velocity.x = direction.x * fasterspeed
-		velocity.z = direction.z * fasterspeed
-	elif is_on_floor() and not Input.is_action_pressed("jump") and direction:
+	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-	elif not is_on_floor() and direction:
-		velocity.x = direction.x * fasterspeed
-		velocity.z = direction.z * fasterspeed
-	elif not is_on_floor() and not direction:
-		velocity.x *= 0.925
-		velocity.z *= 0.925
 	else:
 		velocity.x *= 0.65
 		velocity.z *= 0.65
 	
 	
+	#if direction and is_on_floor() and not Input.is_action_pressed("jump"):
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#elif direction and is_on_floor() and Input.is_action_pressed("jump"):
+		#velocity.z = direction.z * AIRSPEED
+		#velocity.z = direction.z * AIRSPEED
+	#elif direction and not is_on_floor() and Input.is_action_pressed("jump"):
+		#velocity.x = direction.x * AIRSPEED
+		#velocity.z = direction.z * AIRSPEED
+	#elif not direction and not Input.is_action_pressed("jump"):
+		#velocity.x *= 0.65
+		#velocity.z *= 0.65
+
 	move_and_slide()
